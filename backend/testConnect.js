5030aa923b982be9158f86b5d10561bc36d5cb63
Hilowle <mrh27296@gmail.com>
2025-08-03 00:59:43 +0300

    fork first one

--- backend/testConnect.js
+++ backend/testConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb://127.0.0.1:27017/todo-app')
 .then(() => console.log('✅ Connected to local MongoDB!'))
 .catch(err => console.error('❌ Connection failed:', err.message));
